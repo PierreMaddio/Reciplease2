73df5d5ce2215910cb1ffdae919bfb5f922b9950
PierreMaddio <pmsymf@gmail.com>
2022-07-15 21:28:24 +0200

    first commit

---
+++ Reciplease2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease2
+//
+//  Created by Pierre on 15/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
