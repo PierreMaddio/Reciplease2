47e862928c6049e26887932dd224af1672b8e7dd
PierreMaddio <pmsymf@gmail.com>
2022-07-30 11:50:14 +0200

    add RecipeService Alamofire

--- Reciplease2/Controller/ListRecipesViewController.swift
+++ Reciplease2/Controller/ListRecipesViewController.swift
@@ -7,25 +7,43 @@
 
 import UIKit
 
-class ListRecipesViewController: UIViewController {
+class ListRecipesViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableRecipes: UITableView!
+    var recipes = [RecipeClass]()
+    var ingredients: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableRecipes.dataSource = self
+        
+        let service = RecipeService(url: "https://api.edamam.com/api/recipes/v2?q=\(ingredients.joined(separator: ","))&app_key=c4ed16eb0795215c47de2959c93d00c4&app_id=7d167a47&type=any")
+        
+        service.getRecipes()
+        service.completionHandler { [weak self] recipes, status, message in
+            guard let self = self else { return }
+            guard status else { return }
+            guard let _recipes = recipes else { return }
+            self.recipes = _recipes.hits.map { $0.recipe }
+            self.tableRecipes.reloadData()
+        }
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "recipeCell")
+        }
+        let recipe = recipes[indexPath.row]
+        cell?.textLabel?.text = recipe.label
+        
+        return cell!
+    }
 
 }
