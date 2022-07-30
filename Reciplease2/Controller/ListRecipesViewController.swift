84f7f5d9ed6f4f4bb00f08ccb51ce6e92e0531b9
PierreMaddio <pmsymf@gmail.com>
2022-07-30 11:36:53 +0200

    add @IBOutlet tableRecipes

--- Reciplease2/Controller/ListRecipesViewController.swift
+++ Reciplease2/Controller/ListRecipesViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ListRecipesViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableRecipes: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Reciplease2/Network/RecipeService.swift
+++ Reciplease2/Network/RecipeService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import Alamofire
+
+class RecipeService {
+    
+    private let url: String
+    typealias recipesCallBack = (_ recipes: Recipe?, _ status: Bool, _ message: String) -> Void
+    var callBack: recipesCallBack?
+    
+    init(url: String) {
+        self.url = url
+    }
+    
+    // MARK:- getRecipes
+    func getRecipes() {
+        AF.request(
+            self.url,
+            method: .get,
+            parameters: nil,
+            encoding: URLEncoding.default,
+            headers: nil,
+            interceptor: nil).response {
+                (responseData) in
+                guard let data = responseData.data else {
+                    self.callBack?(nil, false, "")
+                    return }
+                do {
+                    let recipes = try JSONDecoder().decode(Recipe.self, from: data)
+                    self.callBack?(recipes, true, "")
+                } catch {
+                    self.callBack?(nil, false, error.localizedDescription)
+                }
+            }
+    }
+    
+    func completionHandler(callBack: @escaping recipesCallBack) {
+        self.callBack = callBack
+    }
+}
