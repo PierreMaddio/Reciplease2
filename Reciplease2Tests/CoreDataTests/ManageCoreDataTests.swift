3f640ea529a8ca070dd0af728a5726691db67eb3
PierreMaddio <pmsymf@gmail.com>
2022-08-28 18:30:34 +0200

    add folder CoreDataTests

--- Reciplease2Tests/CoreDataTests/ManageCoreDataTests.swift
+++ Reciplease2Tests/CoreDataTests/ManageCoreDataTests.swift
@@ -21,12 +21,6 @@ class ManageCoreDataTests: XCTestCase {
         manageCoreData = ManageCoreData(container: coreDataStack.storeContainer)
     }
     
-//        override func tearDown() {
-//          super.tearDown()
-//          manageCoreData = nil
-//          coreDataStack = nil
-//        }
-    
     func testAddAndDeleteFavorite() {
         guard let sut = manageCoreData else { return }
         let label = UUID().uuidString
@@ -93,50 +87,6 @@ class ManageCoreDataTests: XCTestCase {
     }
 }
 
-//class ManageCoreDataTests: XCTestCase {
-//    var persistentContainer: NSPersistentContainer!
-//    override func setUpWithError() throws {
-//        try super.setUpWithError()
-//        continueAfterFailure = false
-//
-//        persistentContainer = .inMemoryContainer()
-//    }
-//
-//    func testAddFavorite() {
-//        let sut = ManageCoreData(container: persistentContainer)
-//        let label = UUID().uuidString
-//        let recipe = makeRecipe(label: label)
-//        sut.markAsFavorite(recipe: recipe) { isFavorite in
-//            //if isFavorite {
-//            print("Calling")
-//            let savedToFavorite = sut.checkIfFavorite(recipeName: label)
-//            XCTAssertTrue(savedToFavorite)
-//            //}
-//
-//            sut.deleteFromFavorite(recipeName: label) {success in
-//                print("Calling... 22")
-//                let savedToFavorite = sut.checkIfFavorite(recipeName: label)
-//                XCTAssertFalse(savedToFavorite)
-//            }
-//        }
-//    }
-//
-//    func makeRecipe(label: String) -> Recipe {
-//        return Recipe(label: label, image: UUID().uuidString, url: UUID().uuidString, yield: Int.random(in: 0...50), ingredientLines: [UUID().uuidString], totalTime: 0)
-//    }
-//}
-//
-//extension NSPersistentContainer {
-//    static func inMemoryContainer() -> NSPersistentContainer {
-//        let desc = NSPersistentStoreDescription()
-//        desc.type = NSInMemoryStoreType
-//
-//        let container = NSPersistentContainer(name: CoreDataStack.modelName,
-//                                              managedObjectModel: CoreDataStack.model)
-//        container.persistentStoreDescriptions = [desc]
-//        return container
-//    }
-//}
 
 
 
